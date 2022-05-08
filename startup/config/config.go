e727cd4c7915723aa7b92acf08f355e192a60c10
MilosZivic75 <zivic.ra75.2018@uns.ac.rs>
2022-05-08 13:29:43 +0200

    Docker update

--- startup/config/config.go
+++ startup/config/config.go
@@ -1,5 +1,10 @@
 package config
 
+import (
+	"fmt"
+	"os"
+)
+
 type Config struct {
 	Port          string
 	Neo4jPort     string
@@ -10,12 +15,27 @@ type Config struct {
 }
 
 func NewConfig() *Config {
-	return &Config{
-		Port:          "8004",
-		Neo4jPort:     "7687",
-		Neo4jHost:     "localhost",
-		Neo4jProtocol: "bolt",
-		Neo4jUsername: "neo4j",
-		Neo4jPassword: "password",
+	if _, err := os.Stat("/.dockerenv"); err == nil {
+		fmt.Println("docker")
+
+		return &Config{
+			Port:          "8000",
+			Neo4jPort:     "7687",
+			Neo4jHost:     "connection-db",
+			Neo4jProtocol: "bolt",
+			Neo4jUsername: "neo4j",
+			Neo4jPassword: "neo4j",
+		}
+	} else {
+		fmt.Println("local")
+
+		return &Config{
+			Port:          "8004",
+			Neo4jPort:     "7687",
+			Neo4jHost:     "localhost",
+			Neo4jProtocol: "bolt",
+			Neo4jUsername: "neo4j",
+			Neo4jPassword: "password",
+		}
 	}
 }
